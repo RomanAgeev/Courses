de439a0434ce62426e78e229f70ba066d8d66c51
Roman Ageev <roman.ageyev@devexpress.com>
2019-12-08 18:20:30 -0500

    Api initial commit

---
+++ Courses.Api/src/Controllers/ValuesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace Courses.Api.Controllers {
+    public class Message {
+        public string Value { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase, IDisposable {
+        public ValuesController() {
+            var factory = new ConnectionFactory {
+                HostName = "localhost"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(
+                queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+
+        readonly IConnection _connection;
+        readonly IModel _channel;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post(Message msg) {
+            string message = msg.Value;
+                    
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(
+                exchange: "",
+                routingKey: "hello",
+                basicProperties: null,
+                body: body
+            );
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public void Dispose() {
+            _channel.Dispose();
+            _connection.Dispose();
+        }
+    }
+}
