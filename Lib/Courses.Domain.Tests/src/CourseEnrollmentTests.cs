862e7838bd7d293c8cb2a77de72101ffb088b261
Roman Ageev <roman.ageyev@devexpress.com>
2019-12-12 18:48:56 -0500

    CourseEnrollment tests

--- Lib/Courses.Domain.Tests/src/CourseEnrollmentTests.cs
+++ Lib/Courses.Domain.Tests/src/CourseEnrollmentTests.cs
@@ -4,7 +4,7 @@ using FluentAssertions;
 namespace Courses.Domain.Tests {
     public class CourseEnrollmentTests {
         [Fact]
-        public void AddStudentTest() {
+        public void AddStudent_Test() {
             var enrollment = new CourseEnrollment(
                 courseId:       "Course_1",
                 courseVersion:  0,
@@ -19,7 +19,7 @@ namespace Courses.Domain.Tests {
                 version: 0,
                 name: "Student 1",
                 age: 30,
-                email: "stident1@test.com"
+                email: "student1@test.com"
             );
 
             enrollment.AddStudent(student);
@@ -30,6 +30,52 @@ namespace Courses.Domain.Tests {
             enrollment.Capacity.Should().Be(2);
             enrollment.Students.Should().BeEquivalentTo(new[] { "Student_1" });
             enrollment.Summary.StudentCount.Should().Be(1);
+        }
+
+        [Fact]
+        public void AddStudentNoCapacity_Test() {
+            var enrollment = new CourseEnrollment(
+                courseId:       "Course_1",
+                courseVersion:  0,
+                courseTitle:    "Course 1",
+                capacity:       2,
+                students:       new string[] { "Student_1", "Student_2" },
+                summary:        new CourseSummary()
+            );
+
+            var student = new Student(
+                id: "Student_3",
+                version: 0,
+                name: "Student 3",
+                age: 30,
+                email: "student3@test.com"
+            );
+
+            enrollment.Invoking(it => it.AddStudent(student))
+                .Should().Throw<DomainException>();
+        }
+
+        [Fact]
+        public void AddStudentDuplicated_Test() {
+              var enrollment = new CourseEnrollment(
+                courseId:       "Course_1",
+                courseVersion:  0,
+                courseTitle:    "Course 1",
+                capacity:       2,
+                students:       new string[] { "Student_1" },
+                summary:        new CourseSummary()
+            );
+
+            var student = new Student(
+                id: "Student_1",
+                version: 0,
+                name: "Student 1",
+                age: 30,
+                email: "student1@test.com"
+            );
+
+            enrollment.Invoking(it => it.AddStudent(student))
+                .Should().Throw<DomainException>();
         }
     }
 } 
